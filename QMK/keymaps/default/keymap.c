da6caa4c8cf9cbc1cafe65e86427abc9567935f7
melonbred <61572569+melonbred@users.noreply.github.com>
2020-04-16 20:17:28 -0700

    Initial Commit

---
+++ QMK/keymaps/default/keymap.c
@@ -0,0 +1,60 @@
+/* Copyright 2020 melonbred
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+
+// Tap Dance Declarations
+enum {
+  TD_M_D = 0,
+  TD_P_M
+};
+
+// Tap Dance Definition
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for minus, tap twice for divide
+  [TD_M_D] = ACTION_TAP_DANCE_DOUBLE(KC_PMNS, KC_PSLS),
+  //Tap once for plus, tap twice for multiply
+  [TD_P_M] = ACTION_TAP_DANCE_DOUBLE(KC_PPLS, KC_PAST)
+};
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT( 
+
+    KC_KP_7, KC_KP_8, KC_KP_9,    TD(TD_M_D),    KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
+    KC_KP_4, KC_KP_5, KC_KP_6,    TD(TD_P_M),    CTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT, \
+    KC_KP_1, KC_KP_2, KC_KP_3,    KC_UP,         KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT, \
+    KC_KP_0, KC_KP_DOT,  KC_LEFT, KC_DOWN, KC_RIGHT,  KC_RGUI, KC_RALT, LT(1, KC_SPC), LT(2, KC_SPC), KC_RALT, KC_SLSH, KC_BSLS \
+    
+  ),
+
+  [1] = LAYOUT( 
+      KC_NO, KC_NO, KC_NO,   KC_NO,     KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+      KC_NO, KC_NO, KC_NO,   KC_NO,     KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_NO, \
+      KC_NO, KC_NO, KC_NO,   KC_NO,     KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SCLN, KC_QUOT, KC_RSFT, \
+      KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO,    KC_NO, KC_NO,      KC_NO,          KC_NO,             KC_NO, KC_NO, KC_NO \
+
+  ),
+
+  [2] = LAYOUT( 
+      KC_NO, KC_NO, KC_NO,   KC_VOLU,     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
+      KC_NO, KC_NO, KC_NO,   KC_VOLD,     KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+      KC_NO, KC_NO, KC_NO,   KC_NO,      KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+      KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO,    KC_NO,  KC_NO,      KC_NO,          KC_NO,      KC_NO, KC_NO, LCA(KC_DEL) \
+  
+  )
+};
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# LHM18
- Left Hand Mini 1800
+# LHM18 Keyboard
+ 
+ ![lhm18](https://i.imgur.com/5vb7BaM.jpg)
+
+  This is a Left Hand Mini 1800 style keyboard that was my first project to learn PCB design through KiCAD. The PCB design is based around a ProMicro and daughter board which uses USB mini-B similar to the [Tanuki Keyboard](https://github.com/SethSenpai/Tanuki). The PCB only supports the layout that is shown in the image above. Please feel free to modify the PCB or case files to fit your needs. 
+
+ # Case
+
+ The case is a typical top mounted desin with the intention for it to be 3D printed with the mounting solution being M2 heat-set inserts and screws. I sourced my hardware from McMaster-Carr:
+
+ [M2 Heat-Set Insert](https://www.mcmaster.com/94180a307) qty 10
+
+ [M2 Screws](https://www.mcmaster.com/92000a012) qty 10
